deac3835a4978123488fcf12d43d0190c38954a9
Davi Ribeiro <davi.rb@hotmail.com>
2023-10-16 22:45:26 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,41 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo Homem de Ferro em 3D",
+    "description": "Marvel anuncia novo homem de ferro",
+    "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=904&height=508&format=webp&quality=91&imagick=/wp-content/uploads/2021/02/homem-de-ferro-tony-divulgacao.jpg"
+
+  },
+
+  {
+    "id": "2",
+    "title": "Matthew Vaughn revela o que o fez abandonar X-Men 3: O Confronto Final",
+    "description": "Olá mundo",
+    "photoCover": "https://static.itapemafm.com.br/s3fs-public/styles/itapema_blog_post_header/public/graphql-upload-files/x-men%20o%20filme%202000_3.jpg?2BwnOfVHawEDmhnn.4VoxxxN2z6aumkG&itok=YkT25S-z"
+
+  },
+
+  {
+    "id": "3",
+    "title": "Disney+: todos os lançamentos de agosto de 2023",
+    "description": "Olá mundo",
+    "photoCover": "https://www.mundoconectado.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=webp&quality=91&imagick=uploads.mundoconectado.com.br/2023/08/10122833/poster-de-guardioes-da-galaxia-vol-3.jpg"
+
+  },
+
+  {
+    "id": "4",
+    "title": "Marvel confirma em HQ qual herói do MCU é tão forte quanto o Hulk",
+    "description": "Olá mundo",
+    "photoCover": "https://t.ctcdn.com.br/xUKjosT-cNCmelSbZQ7frkZkwkg=/768x432/smart/i810767.jpeg"
+
+  },
+
+  {
+    "id": "5",
+    "title": "",
+    "description": "Olá mundo",
+    "photoCover": ""
+
+  }
+]
